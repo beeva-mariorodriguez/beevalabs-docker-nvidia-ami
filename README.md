66b2fe5ca9d8b11ad64bed73bcdd14fa9c944b3c
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-07-13 16:38:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# beevalabs-k8s-gpu-ami
+
+packer template for building an aws image based on kubernetes 1.6 AMI plus nvidia drivers, cuda and nvidia-docker
+
---
+++ beevalabs-k8s-1.6-gpu-xenial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=noninteractive
+
+echo "deb http://httpredir.debian.org/debian jessie-backports main contrib non-free" > /etc/apt/sources.list.d/backports.list
+apt update
+
+# install nvidia drivers & CUDA
+echo 'install nvidia drivers & cuda'
+yes N | apt install --no-upgrade -y -t jessie-backports nvidia-driver nvidia-smi libcuda1
+
+# nvidia-docker, manual installation
+echo 'install nvidia-docker'
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1_amd64.tar.xz
+tar --strip-components=1 -C /usr/bin -xvf /tmp/nvidia-docker*.tar.xz
+cp /usr/lib/x86_64-linux-gnu/libcuda.so* /usr/lib/nvidia/
+
+cp /tmp/nvidia-docker.service /lib/systemd/system/nvidia-docker.service
+useradd -r -M -d /var/lib/nvidia-docker -s /usr/sbin/nologin -c "NVIDIA Docker plugin" nvidia-docker
+setcap cap_fowner+pe /usr/bin/nvidia-docker-plugin
+
+systemctl daemon-reload
+systemctl enable nvidia-docker
+
