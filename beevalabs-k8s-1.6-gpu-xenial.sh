36cddace38b45970e70f0afc9d9de7c8ba2b0843
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-07-17 16:59:18 +0200

    do not install nvidia-docker

--- beevalabs-k8s-1.6-gpu-xenial.sh
+++ beevalabs-k8s-1.6-gpu-xenial.sh
@@ -8,19 +8,7 @@ apt update
 # install nvidia drivers & CUDA
 echo 'install nvidia drivers & cuda'
 yes N | apt install --no-upgrade -y -t jessie-backports nvidia-driver nvidia-smi libcuda1
+cp /usr/lib/x86_64-linux-gnu/libcuda.so* /usr/lib/nvidia/
 # if resolvconf is not installed, resolv.conf wont update correctly via DHCP
 apt install -y resolvconf
-
-# nvidia-docker, manual installation
-echo 'install nvidia-docker'
-wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1_amd64.tar.xz
-tar --strip-components=1 -C /usr/bin -xvf /tmp/nvidia-docker*.tar.xz
-cp /usr/lib/x86_64-linux-gnu/libcuda.so* /usr/lib/nvidia/
-
-cp /tmp/nvidia-docker.service /lib/systemd/system/nvidia-docker.service
-useradd -r -M -d /var/lib/nvidia-docker -s /usr/sbin/nologin -c "NVIDIA Docker plugin" nvidia-docker
-setcap cap_fowner+pe /usr/bin/nvidia-docker-plugin
-
-systemctl daemon-reload
-systemctl enable nvidia-docker
 
