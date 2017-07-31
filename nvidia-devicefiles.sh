8055e5a68ff34099ea762da0ec98aa73a3fc74c9
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-07-31 11:10:25 +0200

    make shellcheck happy (nvidia-devicefiles.sh)

--- nvidia-devicefiles.sh
+++ nvidia-devicefiles.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-/sbin/modprobe nvidia
-
-if [ "$?" -eq 0 ]; then
+if /sbin/modprobe nvidia
+then
   # Count the number of NVIDIA controllers found.
-  NVDEVS=`lspci | grep -i NVIDIA`
-  N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
-  NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
+  NVDEVS=$(lspci | grep -i NVIDIA)
+  N3D=$(echo "$NVDEVS" | grep -c "3D controller")
+  NVGA=$(echo "$NVDEVS" | grep -c "VGA compatible controller")
 
-  N=`expr $N3D + $NVGA - 1`
-  for i in `seq 0 $N`; do
-    mknod -m 666 /dev/nvidia$i c 195 $i
+  N=$(( N3D + NVGA - 1 ))
+  for i in $(seq 0 $N)
+  do
+    mknod -m 666 "/dev/nvidia$i" c 195 "$i"
   done
 
   mknod -m 666 /dev/nvidiactl c 195 255
@@ -19,13 +19,13 @@ else
   exit 1
 fi
 
-/sbin/modprobe nvidia-uvm
-
-if [ "$?" -eq 0 ]; then
+if /sbin/modprobe nvidia-uvm
+then
   # Find out the major device number used by the nvidia-uvm driver
-  D=`grep nvidia-uvm /proc/devices | awk '{print $1}'`
+  D=$(grep nvidia-uvm /proc/devices | awk '{print $1}')
 
-  mknod -m 666 /dev/nvidia-uvm c $D 0
+  mknod -m 666 /dev/nvidia-uvm c "$D" 0
 else
   exit 1
 fi
+
