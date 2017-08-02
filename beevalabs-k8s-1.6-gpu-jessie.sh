6249688e8cc74463f5c27af3bd0dd9a67caef9ee
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-08-02 12:00:24 +0200

    add /usr/local/nvidia
    nvidia drivers and utilities, to be mounted by containers if needed

--- beevalabs-k8s-1.6-gpu-jessie.sh
+++ beevalabs-k8s-1.6-gpu-jessie.sh
@@ -8,7 +8,14 @@ apt update
 # install nvidia drivers & CUDA
 echo 'install nvidia drivers & cuda'
 yes N | apt install --no-upgrade -y -t jessie-backports nvidia-driver nvidia-smi libcuda1 nvidia-cuda-mps
-cp /usr/lib/x86_64-linux-gnu/libcuda.so* /usr/lib/nvidia/
+#
+mkdir -p /usr/local/nvidia/lib64
+mkdir -p /usr/local/nvidia/bin
+cp /usr/lib/x86_64-linux-gnu/libcuda.so.1 /usr/local/nvidia/lib64/
+ln -s libcuda.so /usr/local/nvidia/lib64/libcuda.so.1
+cp /usr/lib/x86_64-linux-gnu/libnvidia-fatbinaryloader.so.375.66 /usr/local/nvidia/lib64/
+cp /usr/lib/x86_64-linux-gnu/libnvidia-ml.so.1 /usr/local/nvidia/lib64/
+cp /usr/bin/nvidia-smi /usr/local/nvidia/bin
 # if resolvconf is not installed, resolv.conf wont update correctly via DHCP
 apt install -y resolvconf
 
