a0b09d222d1fbe5443eba4e99c61f26a4e763736
Mario Rodríguez <mario.rodriguez@beeva.com>
2017-11-15 10:21:23 +0100

    remove nvidia-docker
    nvidia-docker requires a more modern docker than the one installed in
    the source image (k8s-1.6-jessie...) but kubernetes bootstrap dies if
    uninstall docker-engine to install docker-ce ...
    so, if no nvidia-docker we have to:
    * manually populate /usr/local/nvidia
    * create device files with nvidia-devicefiles.sh
    * use docker-engine instead of docker-ce

--- setup_vm.sh
+++ setup_vm.sh
@@ -5,25 +5,19 @@ export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=noninteractive
 
 echo "deb http://httpredir.debian.org/debian jessie-backports main contrib non-free" > /etc/apt/sources.list.d/backports.list
-echo "deb [arch=amd64] https://download.docker.com/linux/debian jessie stable" >> /etc/apt/sources.list.d/docker.list
-
-wget https://download.docker.com/linux/debian/gpg
-apt-key add gpg
 
 apt-get update
 
-# install docker CE
-apt-get purge -y docker-engine
-apt-get install -y docker-ce
-adduser admin docker
 # install nvidia drivers & CUDA
 echo 'install nvidia drivers & cuda'
 yes N | apt-get install --no-upgrade -y -t jessie-backports nvidia-driver nvidia-smi libcuda1 nvidia-cuda-mps
-# install nvidia-docker
-wget https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
-systemctl mask nvidia-docker.service
-dpkg -i nvidia-docker_1.0.1-1_amd64.deb
-systemctl unmask nvidia-docker.service
-systemctl enable nvidia-docker.service
+# /usr/local/nvidia
+mkdir -p /usr/local/nvidia/lib64
+mkdir -p /usr/local/nvidia/bin
+cp /usr/lib/x86_64-linux-gnu/libcuda.so.1 /usr/local/nvidia/lib64/
+ln -s libcuda.so.1 /usr/local/nvidia/lib64/libcuda.so
+cp /usr/lib/x86_64-linux-gnu/libnvidia-fatbinaryloader.so.* /usr/local/nvidia/lib64/
+cp /usr/lib/x86_64-linux-gnu/libnvidia-ml.so.1 /usr/local/nvidia/lib64/
+cp /usr/bin/nvidia-smi /usr/local/nvidia/bin
 # resolvconf
 apt-get install -y resolvconf
