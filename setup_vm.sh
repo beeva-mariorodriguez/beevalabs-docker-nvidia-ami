58c6ad6754b4d9de0a94a78d11a383d083acafa1
beeva-mariorodriguez <mario.rodriguez@beeva.com>
2017-12-05 17:14:24 +0100

    stretch image for k8s with nvidia drivers
    base image: 383156758163/k8s-1.8-debian-stretch-amd64-hvm-ebs-2017-12-02

--- setup_vm.sh
+++ setup_vm.sh
@@ -8,16 +8,6 @@ echo "deb http://cdn-aws.deb.debian.org/debian stretch main non-free contrib" > 
 echo "deb http://security.debian.org stretch/updates main non-free contrib" >> /etc/apt/sources.list
 echo "deb http://cdn-aws.deb.debian.org/debian stretch-updates main non-free contrib" >> /etc/apt/sources.list
 
-echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" >> /etc/apt/sources.list.d/docker.list
-
-wget https://download.docker.com/linux/debian/gpg
-apt-key add gpg
-apt-get update
-
-# install docker
-echo 'installing docker'
-apt-get install -y docker-ce
-adduser admin docker
 # install nvidia drivers & CUDA
 echo 'installing nvidia drivers & cuda'
 apt-get install -y nvidia-driver nvidia-smi libcuda1 nvidia-cuda-mps
